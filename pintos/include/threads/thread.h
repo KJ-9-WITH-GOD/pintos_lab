eedcdfc081266fe0c2c0906fcc0809a8d3451274
Jinwoo Han <128575512+GigaGukBab@users.noreply.github.com>
2025-07-30 21:38:15 +0900

    프로세스 관리 및 시스템 콜 구현 (fork, exec, wait, exit, 파일 관련 syscall), 유저 프로세스 및 파일 디스크립터 테이블 기능 확장 및 구조 정리
    * chore: format all files
    
    * feat(open): implement open syscall with partial test pass
    
    - Implemented process_addfile() and tintegrated SYS_OPEN handler
    - Passed open-normal, open-missing, open-boundary, open-empty, open-twice
    
    * feat: implement create syscall
     - test case for create-bad-ptr, create-null not passed
    
    * feat: implement close syscall
     - checkpoint for debug
    
    * fix: add missing function
    
    * fix: fix header including issue
    
    * feat: implement file descriptor table
     - handle process exit
     - handle stdin, stdout initialization on fdt
    
    * feat: implement syscall "create"
     - add logic for bad_address test case
    
    * feat: implement close system call
    
    * refactor: migrate from list-based fd management and pass null/bad-ptr open tests
    - Replaced list-based file descriptor management with fd table
    - Updated related logic in open syscall to use fd table
    - Passed open-null and open-bad-ptr tests
    
    * feat: implement filesize system call handler
    
    * feat: implement read system call handler
    
    * feat: implement write system call handler
    
    * feat: 프로세스 관리 및 시스템 콜 구현 (fork, exec, wait, exit, 파일 관련 syscall) (#1)
    
    * checkpoint: fork-once 작업 중 오류 작업 중
    
    * checkpoint: fork-once 구현 중간 저장 및 오류 디버깅 (#23)
    
    * feat: implement filesize system call handler
    
    * feat: implement read system call handler
    
    * feat: implement write system call handler
    
    * checkpoint: fork-once 작업 중 오류 작업 중
    
    * feat: pass test case from 1 to 76
    
    ---------
    
    Co-authored-by: Roar Jang <roarjang@gmail.com>
    Co-authored-by: Yerinnnn <shinyerin0216@gmail.com>
    Co-authored-by: Shin Yerin <37470054+Yerinnnn@users.noreply.github.com>

--- pintos/include/threads/thread.h
+++ pintos/include/threads/thread.h
@@ -4,7 +4,9 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
+
 #include "threads/interrupt.h"
+#include "threads/synch.h"
 #ifdef VM
 #include "vm/vm.h"
 #endif
@@ -12,21 +14,40 @@
 /* States in a thread's life cycle. */
 enum thread_status
 {
-	THREAD_RUNNING, /* Running thread. */
-	THREAD_READY,		/* Not running but ready to run. */
-	THREAD_BLOCKED, /* Waiting for an event to trigger. */
-	THREAD_DYING		/* About to be destroyed. */
+    THREAD_RUNNING, /* Running thread. */
+    THREAD_READY,   /* Not running but ready to run. */
+    THREAD_BLOCKED, /* Waiting for an event to trigger. */
+    THREAD_DYING    /* About to be destroyed. */
+};
+
+/* File descriptor table size. */
+#define FDTABLE_SIZE 64
+
+enum fd_type
+{
+    FD_STDIN,
+    FD_STDOUT,
+    FD_FILE,
+    FD_DIR,
+};
+
+/* universal file object which contains file type and pointer
+ * address(file or dir) if it's not STDIN or STDOUT */
+struct uni_file
+{
+    enum fd_type type;
+    void *ptr;
 };
 
 /* Thread identifier type.
-	 You can redefine this to whatever type you like. */
+         You can redefine this to whatever type you like. */
 typedef int tid_t;
 #define TID_ERROR ((tid_t) - 1) /* Error value for tid_t. */
 
 /* Thread priorities. */
-#define PRI_MIN 0			 /* Lowest priority. */
+#define PRI_MIN 0      /* Lowest priority. */
 #define PRI_DEFAULT 31 /* Default priority. */
-#define PRI_MAX 63		 /* Highest priority. */
+#define PRI_MAX 63     /* Highest priority. */
 
 /* Global tick - min value of sleeping thread's wakeup tick */
 extern int64_t min_tick;
@@ -90,35 +111,47 @@ extern int64_t min_tick;
  * blocked state is on a semaphore wait list. */
 struct thread
 {
-	/* Owned by thread.c. */
-	tid_t tid;								 /* Thread identifier. */
-	enum thread_status status; /* Thread state. */
-	char name[16];						 /* Name (for debugging purposes). */
-	int priority;							 /* Priority. */
-	int64_t wakeup_tick;			 /* Tick till wake up */
+    /* Owned by thread.c. */
+    tid_t tid;                 /* Thread identifier. */
+    enum thread_status status; /* Thread state. */
+    char name[16];             /* Name (for debugging purposes). */
+    int priority;              /* Priority. */
+    int64_t wakeup_tick;       /* Tick till wake up */
+    struct uni_file
+        *fd_table[FDTABLE_SIZE]; /* file descriptor table for each process */
+    int next_fd;                 /* file descriptor number to allocate file */
+    struct intr_frame parent_if; /* parent's intr frame for fork */
+    struct list child_list;      /* List to manage child process */
+    struct list_elem child_elem; /* list element for child_list */
+    struct semaphore wait_sema /* control wait system call control flow */;
+    struct semaphore fork_sema /* control fork system call control flow */;
+    struct semaphore exit_sema /* control exit system call control flow */;
+    bool load_success;
+    int exit_status;  /* exit code for child process */
+    bool wait_called; /* has this process called wait? */
+    /* this info is used on process_exit. */
+    struct file *running_file; /* current running file */
 
-	/* Shared between thread.c and synch.c. */
-	struct list_elem elem; /* List element. */
-
-	struct list fd_list;
+    /* Shared between thread.c and synch.c. */
+    struct list_elem elem; /* List element. */
 
 #ifdef USERPROG
-	/* Owned by userprog/process.c. */
-	uint64_t *pml4; /* Page map level 4 */
+    /* Owned by userprog/process.c. */
+    uint64_t *pml4; /* Page map level 4 */
 #endif
 #ifdef VM
-	/* Table for whole virtual memory owned by thread. */
-	struct supplemental_page_table spt;
+    /* Table for whole virtual memory owned by thread. */
+    struct supplemental_page_table spt;
 #endif
 
-	/* Owned by thread.c. */
-	struct intr_frame tf; /* Information for switching */
-	unsigned magic;				/* Detects stack overflow. */
+    /* Owned by thread.c. */
+    struct intr_frame tf; /* Information for switching */
+    unsigned magic;       /* Detects stack overflow. */
 };
 
 /* If false (default), use round-robin scheduler.
-	 If true, use multi-level feedback queue scheduler.
-	 Controlled by kernel command-line option "-o mlfqs". */
+         If true, use multi-level feedback queue scheduler.
+         Controlled by kernel command-line option "-o mlfqs". */
 extern bool thread_mlfqs;
 
 void thread_init(void);
@@ -131,11 +164,11 @@ typedef void thread_func(void *aux);
 tid_t thread_create(const char *name, int priority, thread_func *, void *);
 
 bool tick_less(const struct list_elem *a_, const struct list_elem *b_,
-							 void *aux UNUSED);
+               void *aux UNUSED);
 bool priority_large(const struct list_elem *a_, const struct list_elem *b_,
-										void *aux UNUSED);
+                    void *aux UNUSED);
 void thread_sleep(int64_t sleep_ticks);
-void thread_wakeup();
+void thread_wakeup(void);
 void thread_block(void);
 void thread_unblock(struct thread *);
 
@@ -145,6 +178,7 @@ const char *thread_name(void);
 
 void thread_exit(void) NO_RETURN;
 void thread_yield(void);
+void thread_yield_r(void);
 
 int thread_get_priority(void);
 void thread_set_priority(int);
